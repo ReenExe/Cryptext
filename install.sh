c7be3dc18399c597f871b3b1b71c38bde1b36111
ReenExe <ipourri@gmail.com>
2015-05-31 20:19:46 +0300

    hotfix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get install phpunit
-wget -O php/composer.phar https://getcomposer.org/download/1.0.0-alpha10/composer.phar
+wget https://getcomposer.org/download/1.0.0-alpha10/composer.phar
 php composer.phar install
