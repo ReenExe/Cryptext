5f7469d514c204cb358b7c533150683dad636642
ReenExe <ipourri@gmail.com>
2015-05-31 21:53:05 +0300

    add console command

---
+++ app.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use ReenExe\Cryptext\MainCommand;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add(new MainCommand());
+$application->run();
